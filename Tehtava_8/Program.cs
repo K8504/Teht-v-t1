6d35a5862ae7c3b7a8c487e937f1bd0bd412549c
K8504 <k8504@student.jamk.fi>
2017-01-19 15:26:47 +0200

    Tehtava_8

--- Tehtava_8/Program.cs
+++ Tehtava_8/Program.cs
@@ -10,6 +10,18 @@ namespace Tehtava_8
     {
         static void Main(string[] args)
         {
+            int luku1, luku2, luku3;
+
+            Console.WriteLine("Anna kolme lukua: ");
+            luku1 = int.Parse(Console.ReadLine());
+            luku2 = int.Parse(Console.ReadLine());
+            luku3 = int.Parse(Console.ReadLine());
+
+            if (luku1 > luku2 && luku1 > luku3) Console.WriteLine("Suurin luku on " + luku1);
+
+            else if (luku2 > luku1 && luku2 > luku3) Console.WriteLine("Suurin luku on " + luku2);
+
+            else if (luku3 > luku1 && luku3 > luku2) Console.WriteLine("Suurin luku on " + luku3);
         }
     }
 }
