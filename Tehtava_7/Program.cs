3e5171de5eef56a8ea023f96d730581cce84d99c
K8504 <k8504@student.jamk.fi>
2017-01-19 15:06:14 +0200

    Tehtävä7

---
+++ Tehtava_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna vuosi: ");
+            int vuosi;
+            vuosi = int.Parse(Console.ReadLine());
+      
+                if (vuosi % 4 == 0 && vuosi % 100 != 0 || vuosi % 400 == 0) {
+                Console.WriteLine("Vuosi on karkausvuosi");
+
+            } else{
+                Console.WriteLine("Ei ole karkausvuosi");
+            }
+
+        }
+    }
+}
